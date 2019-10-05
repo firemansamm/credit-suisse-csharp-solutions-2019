6ae9e81afbe12098932f513159443677e8b39b44
Sam Gao <gaoshan_274@hotmail.com>
2019-10-05 22:57:24 +0100

    fix knapsack off by 1

--- native.cpp
+++ native.cpp
@@ -21,7 +21,6 @@ int x3[Q3_MAX_SCORE + 10];
 extern "C" int ans3(int* s, int sl, int* y, int yl) {
     sort(s, s + sl);
     int nl = unique(s, s+sl) - s, bs = 0, bc = -1;
-    vector<int> rs(yl);
     for(int i=0;i<yl;i++) {
         int r = (lower_bound(s, s + nl, y[i]) - s);
         int rk = nl - r + (s[r] != y[i]);
@@ -39,9 +38,10 @@ extern "C" int ans3(int* s, int sl, int* y, int yl) {
 int dp4[Q4_MAX_ITEMS + 10][Q4_MAX_CAP + 10];
 extern "C" int ans4(int* v, int *c, int len, int cap) {
     for (int i=1;i<=len;i++) {
-        for (int j=0;j<=cap;j++) {
-            if (j < c[i]) dp4[i][j] = dp4[i-1][j];
-            else dp4[i][j] = max(dp4[i-1][j], dp4[i-1][j-c[i]] + v[i]);
+        int cc = c[i-1], cv = v[i-1];
+        for(int j=0;j<=cap;j++) {
+            if (j < cc) dp4[i][j] = dp4[i-1][j];
+            else dp4[i][j] = max(dp4[i-1][j-cc] + cv, dp4[i-1][j]);
         }
     }
     return dp4[len][cap];
