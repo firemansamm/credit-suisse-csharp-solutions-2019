e714f7dadbf356e01a2e3f202a95fa51b85ce084
Sam Gao <gaoshan_274@hotmail.com>
2019-10-06 00:20:58 +0100

    i don't understand this question

--- Answers/Question6.cs
+++ Answers/Question6.cs
@@ -1,11 +1,28 @@
-﻿namespace C_Sharp_Challenge_Skeleton.Answers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question6
     {
         public static int Answer(string[] input)
         {
-            //TODO: Please work out the solution;
-            return -1;
+            var x = new HashSet<(char, char)>();
+            var ans = -1;
+            for (var i = 0; i < input.Length; i++)
+            {
+                var k = input[i];
+                var g = (k[0], k[k.Length - 1]);
+                if (x.Contains(g))
+                {
+                    ans = i;
+                }
+
+                x.Add(g);
+            }
+
+            return ans;
         }
     }
 }
