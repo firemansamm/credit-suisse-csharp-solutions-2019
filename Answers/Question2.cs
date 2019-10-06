e79e2706004f9b9a8f4a1748c986eadb9bb8503f
Sam Gao <gaoshan_274@hotmail.com>
2019-10-06 16:58:34 +0100

    actually call q2

--- Answers/Question2.cs
+++ Answers/Question2.cs
@@ -1,11 +1,16 @@
-﻿namespace C_Sharp_Challenge_Skeleton.Answers
+﻿using System.Runtime.InteropServices;
+using System.Security;
+
+namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question2
     {
+        [DllImport("native", EntryPoint = "ans2")]
+        [SuppressUnmanagedCodeSecurity]
+        public static extern int NativeAnswer(int[] t, int tl, int[] r, int[] b, int len);
         public static int Answer(int[] risk, int[] bonus, int[] trader)
         {
-            //TODO: Please work out the solution;
-            return -1;
+            return NativeAnswer(trader, trader.Length, risk, bonus, bonus.Length);
         }
     }
 }
