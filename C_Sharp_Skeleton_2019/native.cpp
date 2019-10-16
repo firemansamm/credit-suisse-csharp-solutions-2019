22f25baa0d64a1865a502accf78147200f54f4f9
Sam Gao <gaoshan_274@hotmail.com>
2019-10-16 16:42:21 +0100

    [skip ci] test q4 speed

--- C_Sharp_Skeleton_2019/native.cpp
+++ C_Sharp_Skeleton_2019/native.cpp
@@ -107,17 +107,19 @@ extern "C" int ans3(int* s, int sl, int* y, int yl) {
     return bp;
 }
 
-int dp4[Q4_MAX_ITEMS + 10][Q4_MAX_CAP + 10];
+int dp4[Q4_MAX_CAP + 10];
 extern "C" int ans4(int* v, int *c, int len, int cap) {
     TEST;
-    for (int i=1;i<=len;i++) {
-        int cc = c[i-1], cv = v[i-1];
-        for(int j=0;j<=cap;j++) {
-            if (j < cc) dp4[i][j] = dp4[i-1][j];
-            else dp4[i][j] = max(dp4[i-1][j-cc] + cv, dp4[i-1][j]);
+    memset(dp4, 0, sizeof dp4);
+    for (int i=0;i<len;i++) {
+        for(int j=cap;j>=c[i];j--) {
+            dp4[j] = max(dp4[j], dp4[j-c[i]]+v[i]);
+            /*if (j < cc) dp4[i][j] = dp4[i-1][j];
+            else dp4[i][j] = max(dp4[i-1][j-cc] + cv, dp4[i-1][j]);*/
         }
     }
-    return dp4[len][cap];
+    //return dp4[len][cap];
+    return dp4[cap];
 }
 
 ii d5[Q5_MAX_N + 10];
