9160bbbe7679f492f73fdd3ca7566d23038d3e68
Sam Gao <gaoshan_274@hotmail.com>
2019-10-18 16:30:31 +0100

    could it be?

--- C_Sharp_Skeleton_2019/Answers/Question2.cs
+++ C_Sharp_Skeleton_2019/Answers/Question2.cs
@@ -1,16 +1,38 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 using System.Security;
 
 namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question2
     {
-        [DllImport("native", EntryPoint = "ans2")]
+        /*[DllImport("native", EntryPoint = "ans2")]
         [SuppressUnmanagedCodeSecurity]
-        public static extern int ans2(int[] t, int tl, int[] r, int[] b, int len);
+        public static extern int ans2(int[] t, int tl, int[] r, int[] b, int len);*/
         public static int Answer(int[] risk, int[] bonus, int[] trader)
         {
-            return ans2(trader, trader.Length, risk, bonus, bonus.Length);
+            int[] x = new int[160];
+            int len = risk.Length;
+            for (int i = len - 1; i >= 0; i--)
+            {
+                if (bonus[i] > x[risk[i]]) x[risk[i]] = bonus[i];
+            }
+            Array.Sort(risk);
+            Array.Sort(trader);
+            int dx = 0, ans = 0, cm = 0;
+            for (int i = 0; i < len; i++)
+            {
+                while (dx < len && risk[dx] <= trader[i])
+                {
+                    if (x[risk[dx]] > cm) cm = x[risk[dx]];
+                    dx++;
+                }
+
+                ans += cm;
+            }
+
+            return ans;
+            //return ans2(trader, trader.Length, risk, bonus, bonus.Length);
         }
     }
 }
