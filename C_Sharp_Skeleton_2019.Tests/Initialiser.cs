3a7c7eba923051e7b15db5fadb98858332ba3bf4
Sam Gao <gaoshan_274@hotmail.com>
2019-10-23 13:29:02 +0100

    thing

---
+++ C_Sharp_Skeleton_2019.Tests/Initialiser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using C_Sharp_Challenge_Skeleton.Answers;
+
+namespace C_Sharp_Skeleton_2019
+{
+    public static class Initialiser
+    {
+        public static void Run()
+        {
+            // warms up the methods
+            Question2.Answer(new int[]{0}, new int[]{0}, new int[]{0});
+            Question3.Answer(new int[]{0}, new int[]{0});
+            Question4.Answer(new int[]{0}, new int[]{0}, 0);
+            Question5.Answer(new int[] { 0 });
+        }
+    }
+}
